d097a223b1aa8965cb0bfd3780a9d3a6d2899f49
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2018-12-31 00:54:16 -0500

    slice problem

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,21 @@ module Main where
 import System.IO
 import ListVector
 import ListMatrix
+import TensorIO
 
 mat::ListMatrix.ListMatrix
 mat = [[1,2],[3,4]]
 
+vec::ListVector.ListVector
+vec = [1,2,3,2,4,6]
+
+-- main :: IO ()
+-- main =
+-- 	let av1 = readVector
+-- 	in av1 >>= \a1 ->
+--         let av2 = readVector
+--         in av2 >>= \a2 ->
+--         let mat = av1:av2:[]
+--         in foldr (\v a -> printVector v) mat (putStr "")
 main :: IO ()
-main =  do
-    putStrLn $ show mat
+main = TensorIO.printVector $ ListVector.listVectorSlice 1 0 3 vec
