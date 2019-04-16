fd65c40dcbdc3d6e85d8b30786f5bf2997a22224
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-04-15 23:27:43 -0400

    change row columsn

--- src/ListMatrix.hs
+++ src/ListMatrix.hs
@@ -33,15 +33,15 @@ transpose mat =
 
 listMatrixSlice  :: (Int,Int) -> (Int,Int)-> ListMatrix -> ListMatrix
 listMatrixSlice (rowSliceStart,colSliceStart) (rowSliceLen,colSliceLen) mat=
-    let slicedcols :: ListMatrix = ListVector.listSlice   colSliceStart colSliceLen mat
-    in let f :: (ListVector.ListVector-> ListVector.ListVector) = (ListVector.listVectorSlice rowSliceStart rowSliceLen)
-    in map f slicedcols
-matrixRowMultiSlice ::  Int -> ListMatrix ->[Int] -> [ListMatrix]
-matrixRowMultiSlice  rowLen mat [] = []
-matrixRowMultiSlice  rowLen mat rowSliceStart@(cur:next) =
+    let slicedrows :: ListMatrix = ListVector.listSlice   rowSliceStart rowSliceLen mat
+    in let f :: (ListVector.ListVector-> ListVector.ListVector) = (ListVector.listVectorSlice colSliceStart colSliceLen)
+    in map f slicedrows
+matrixColMultiSlice ::  Int -> ListMatrix ->[Int] -> [ListMatrix]
+matrixColMultiSlice  colLen mat [] = []
+matrixColMultiSlice  colLen mat colSliceStart@(cur:next) =
     let slice1 :: (ListVector.ListVector-> ListVector.ListVector) = (ListVector.listVectorSlice 0 cur)
-    in let slice2 :: (ListVector.ListVector-> ListVector.ListVector) = (ListVector.listVectorSlice (cur Prelude.+1) (rowLen-1-cur))
-    in (zipWith (++) (map slice1 mat) (map slice2 mat)):(matrixRowMultiSlice rowLen mat next)
+    in let slice2 :: (ListVector.ListVector-> ListVector.ListVector) = (ListVector.listVectorSlice (cur Prelude.+1) (colLen-1-cur))
+    in (zipWith (++) (map slice1 mat) (map slice2 mat)):(matrixColMultiSlice colLen mat next)
 
 checkMatrixColsEqualLen::ListMatrix->Bool
 -- ^checkMatrixColsEqualLen function checks if all the columns of a matrix are equal size
@@ -49,32 +49,32 @@ checkMatrixColsEqualLen _ = False
 
 getValidMatrixTotalRowCol :: ListMatrix -> (Int,Int)
 -- ^getValidMatrixRowCol function performs checks to see the validity of the function and returns its number of rows and columns
-getValidMatrixTotalRowCol mat =((length $ head mat),length mat)
+getValidMatrixTotalRowCol mat =(length mat,(length $ head mat))
 
 checkSquareMatrix :: ListMatrix -> Bool
 checkSquareMatrix mat = undefined
 --- a b c
 --- d e f
 --- g h i
--- --> [a,d,g],[b,e,h],[c,f,i]
+-- --> [a,b,c],[d,e,f],[g,h,i]
 
 determinant :: ListMatrix -> Scalar
 determinant [[a]]  = a
--- determinant [[a,c],[b,d]]  = a Prelude.*d - b Prelude.*c
--- determinant  currentMat@[[a,d,g],[b,e,h],[c,f,i]]  =
---     a Prelude.* determinant [[e,h],[f,i]]  - b Prelude.* determinant [[d,g],[f,i]]  Prelude.+ c Prelude.* determinant [[d,g],[e,h]]
+-- determinant [[a,b],[c,d]]  = a Prelude.*d - b Prelude.*c
+-- determinant  currentMat@[[a,b,c],[d,e,f],[g,h,i]]  =
+--     a Prelude.* determinant [[e,f],[h,i]]  - b Prelude.* determinant [[d,g],[f,i]]  Prelude.+ c Prelude.* determinant [[d,g],[e,h]]
 determinant  mat@(firstRow:rest) =
     let (rowNum,colNum) = getValidMatrixTotalRowCol mat
-    in let rowsIdxEnumerated = [0..rowNum-1]
-    in let signs :: [Scalar] = map (\x->(fromIntegral (x`mod`2)) Prelude.*(-2.0)Prelude.+1.0) rowsIdxEnumerated -- 0-> 1,1->-1,2->1,3->-1 ...
-    in let slices = matrixRowMultiSlice rowNum  mat rowsIdxEnumerated -- zipWith ()rowsIdxEnumerated (repeat rest)
+    in let colsIdxEnumerated = [0..rowNum-1]
+    in let signs :: [Scalar] = map (\x->(fromIntegral (x`mod`2)) Prelude.*(-2.0)Prelude.+1.0) colsIdxEnumerated -- 0-> 1,1->-1,2->1,3->-1 ...
+    in let slices = matrixColMultiSlice colNum  mat colsIdxEnumerated -- zipWith ()colsIdxEnumerated (repeat rest)
     in let partialDeterminant :: ListVector= zipWith3 (\s e m-> s Prelude.* e Prelude.* (determinant m)) signs firstRow slices
     in foldr (Prelude.+) 0 partialDeterminant
 
 
 inverseStructure :: ListMatrix -> ListMatrix
-inverseStructure [[a,c],[b,d]] =
-    [[d,-1 Prelude.* c], [-1 Prelude.* b,a]]
+inverseStructure [[a,b],[c,d]] =
+    [[d,-1 Prelude.* b], [-1 Prelude.* c,a]]
 
 mutliplybyvector :: ListVector -> ListMatrix -> ListVector
 mutliplybyvector vec_a b =
